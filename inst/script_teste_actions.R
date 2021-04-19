e833a1c48e83e18afd7051b71d73dc829530a0c2
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-19 15:01:40 -0300

    muda argumento

--- inst/script_teste_actions.R
+++ inst/script_teste_actions.R
@@ -4,5 +4,5 @@ mes <- lubridate::month(data)
 caminho <- glue::glue("inst/dados_teste/{ano}/{mes}")
 
 ComitesBaciaSP::download_html(path = caminho,
-                              siglas_comites = "AT",
+                              sigla_do_comite = "AT",
                               pagina = "atas")
