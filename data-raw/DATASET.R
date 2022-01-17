4f5bb084d1151510220b5f0833e140882a74bd48
Beatriz Milz <milz.bea@gmail.com>
2022-01-17 16:40:50 -0300

    ler atas está completo!

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,28 @@
 ## code to prepare `DATASET` dataset goes here
 
-caminho_arquivos <- tibble::tibble(caminho = list.files("inst/dados_html", pattern = ".html", full.names = TRUE, recursive = TRUE),
-                                   nome_arquivo = list.files("inst/dados_html", pattern = ".html", recursive = TRUE)) |>
+# Função --------
+obter_tabela_atas_comites_modificado <- function(base) {
+  base_lida <- ComitesBaciaSP::obter_tabela_atas_comites(
+    sigla_do_comite = base$comite[1],
+    online = FALSE,
+    path_arquivo = base$caminho[1]
+  )
+
+  base_lida |> readr::write_rds(file = base$caminho_salvar_rds[1])
+}
+
+# Scripts para preparar os dados -----
+
+caminho_arquivos <-
+  tibble::tibble(
+    caminho = list.files(
+      "inst/dados_html",
+      pattern = ".html",
+      full.names = TRUE,
+      recursive = TRUE
+    ),
+    nome_arquivo = list.files("inst/dados_html", pattern = ".html", recursive = TRUE)
+  ) |>
   dplyr::mutate(nome_arquivo = fs::path_file(nome_arquivo)) |>
   tidyr::separate(
     nome_arquivo,
@@ -10,55 +31,50 @@ caminho_arquivos <- tibble::tibble(caminho = list.files("inst/dados_html", patte
     remove = FALSE,
     extra = "drop"
   ) |>
-  dplyr::mutate(ano = stringr::str_remove_all(ano, ".html"))
+  dplyr::mutate(ano = stringr::str_remove_all(ano, ".html")) |>
+  dplyr::mutate(
+    caminho_salvar_rds =  stringr::str_replace(caminho, ".html$", ".Rds"),
+    caminho_salvar_rds =  stringr::str_replace(caminho_salvar_rds, "dados_html", "dados_rds")
+  )
 
 # criar as pastas
 dirname(caminho_arquivos$caminho) |>
   stringr::str_replace("dados_html", "dados_rds") |>
   fs::dir_create()
 
-# filtrar por atas
-atas_ler <- caminho_arquivos |>
-  dplyr::filter(conteudo_de_pagina == "atas") |> head()
+# ver arquivos já lidos
 
-obter_tabela_atas_comites_modificado <- function(base){
-#
-#   base <- atas_ler|>
-#     dplyr::group_split(caminho) |>
-#     purrr::pluck(1)
-
-
-  base_complementar <- base |>
-    dplyr::mutate(caminho_salvar =  stringr::str_replace(caminho, ".html$", ".Rds$"),
-                  caminho_salvar =  stringr::str_replace(caminho_salvar, "dados_html", "dados_rds"))
-
-   base_lida <- ComitesBaciaSP::obter_tabela_atas_comites(
-    sigla_do_comite = base_complementar$comite[1],
-    online = FALSE,
-    path_arquivo = base_complementar$caminho[1]
+arquivos_lidos <-
+  list.files(
+    "inst/dados_rds",
+    pattern = ".Rds",
+    full.names = TRUE,
+    recursive = TRUE
   )
 
-  base_lida |> readr::write_rds(file = base_complementar$caminho_salvar[1])
-}
+caminho_arquivos_nao_lidos <- caminho_arquivos |>
+  dplyr::filter(!caminho_salvar_rds %in% arquivos_lidos)
 
 
+# LER ATAS ------------------------------------
+# filtrar por atas
+atas_ler <- caminho_arquivos_nao_lidos |>
+  dplyr::filter(conteudo_de_pagina == "atas")
+
+# ler atas
 atas_ler |>
   dplyr::group_split(caminho) |>
   purrr::map(obter_tabela_atas_comites_modificado)
 
+# unificar base de atas
+atas_completas <-
+  list.files(
+    "inst/dados_rds",
+    pattern = ".Rds",
+    full.names = TRUE,
+    recursive = TRUE
+  ) |>
+  purrr::map_dfr(readr::read_rds)
 
-
-# atas_lido <-
-#   purrr::map2_dfr(
-#     .x = atas_ler$comite,
-#     .y = atas_ler$caminho,
-#     .f = ~ ComitesBaciaSP::obter_tabela_atas_comites(
-#       sigla_do_comite = .x,
-#       online = FALSE,
-#       path_arquivo = .y
-#     )
-#   )
-
-
-
-usethis::use_data(atas_lido, overwrite = TRUE)
+# exportar base de atas
+usethis::use_data(atas_completas, overwrite = TRUE)
